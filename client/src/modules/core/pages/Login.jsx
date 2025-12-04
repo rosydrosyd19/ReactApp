522e5ac39a3b146df67f64dbe0885dc96bb0d776
rosydrosyd19 <rosydrosyd19@gmail.com>
2025-12-04 13:29:40 +0700

    login fix

--- client/src/modules/core/pages/Login.jsx
+++ client/src/modules/core/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
         setLoading(true);
 
         try {
-            const response = await axios.post('http://localhost:5000/api/auth/login', {
+            const response = await axios.post('/api/auth/login', {
                 email,
                 password,
                 rememberMe
