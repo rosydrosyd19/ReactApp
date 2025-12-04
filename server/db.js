995fd93fb86610b79f5437371872942172de21a8
rosydrosyd19 <rosydrosyd19@gmail.com>
2025-12-04 13:03:11 +0700

    update database

--- server/db.js
+++ server/db.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '', // Default XAMPP password
-    database: 'asset_management_db'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'asset_management_db'
 });
 
 db.connect((err) => {
